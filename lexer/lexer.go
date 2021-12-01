74520c347fbd11517eae57cd9b6095c5b04b8ce4
petrostrak <pit.trak@gmail.com>
2021-12-01 20:01:05 +0200

    Add NextToken method

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/petrostrak/the-monkey-programming-language/token"
+
 type Lexer struct {
 	input        string
 	position     int  // current position in input (points to current char).
@@ -27,4 +29,41 @@ func (l *Lexer) readChar() {
 
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+// NextToken looks at the current character under examination (l.ch) and
+// returns a token depending on which character it is.
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	}
+
+	// before returning the token, we advance our pointers into the input
+	// so when we call NextToken() again the l.ch field is already updated.
+	l.readChar()
+	return tok
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
