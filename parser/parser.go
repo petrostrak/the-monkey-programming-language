fceca03d7b555b9d167545c5197772063d531f4d
petrostrak <pit.trak@gmail.com>
2021-12-05 20:23:00 +0200

    Add error field in Parser struct

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/petrostrak/the-monkey-programming-language/ast"
 	"github.com/petrostrak/the-monkey-programming-language/lexer"
 	"github.com/petrostrak/the-monkey-programming-language/token"
@@ -10,10 +12,14 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// read two tokens, so curToken and peekToken are both set
 	p.nextToken()
@@ -100,4 +106,13 @@ func (p *Parser) ParseProgram() *ast.Program {
 	}
 
 	return program
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
