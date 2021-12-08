d23a0d8878c879d8fa5b52804bab37f00191c64d
petrostrak <pit.trak@gmail.com>
2021-12-08 16:13:06 +0200

    Prefix Operators - testIntegerLiteral method

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/petrostrak/the-monkey-programming-language/ast"
@@ -173,6 +174,22 @@ func TestIntegerLiteralExpression(t *testing.T) {
 }
 
 func testIntegerLiteral(t *testing.T, il ast.Expression, value int64) bool {
+	integ, ok := il.(*ast.IntegerLiteral)
+	if !ok {
+		t.Errorf("il not *ast.IntegerLiteral; got %T", il)
+		return false
+	}
+
+	if integ.Value != value {
+		t.Errorf("integ.Value not %d; got %d", value, integ.Value)
+		return false
+	}
+
+	if integ.TokenLiteral() != fmt.Sprintf("%d", value) {
+		t.Errorf("integ.TokenLitera() no %d; got %s", value, integ.TokenLiteral())
+		return false
+	}
+
 	return true
 }
 
