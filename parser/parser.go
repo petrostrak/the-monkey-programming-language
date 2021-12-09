ed5d72a1aadb916e4b8b4df0089a47d38a65ddf9
petrostrak <pit.trak@gmail.com>
2021-12-09 15:43:02 +0200

    Add parseInfixExpression method

--- parser/parser.go
+++ parser/parser.go
@@ -270,3 +270,17 @@ func (p *Parser) curPrecedence() int {
 
 	return LOWEST
 }
+
+func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
+	expression := &ast.InfixExpression{
+		Token:    p.curToken,
+		Operator: p.curToken.Literal,
+		Left:     left,
+	}
+
+	precedence := p.curPrecedence()
+	p.nextToken()
+	expression.Right = p.parseExpression(precedence)
+
+	return expression
+}
