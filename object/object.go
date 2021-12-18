83c6a1cb808593a759ae5a981f23bbe762198be4
petrostrak <pit.trak@gmail.com>
2021-12-18 12:22:26 +0200

    Implement Boolean struct

--- object/object.go
+++ object/object.go
@@ -4,6 +4,7 @@ import "fmt"
 
 const (
 	INTEGER_OBJ = "INTEGER"
+	BOOLEAN_OBJ = "BOOLEAN"
 )
 
 type ObjectType string
@@ -26,3 +27,10 @@ type Integer struct {
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
