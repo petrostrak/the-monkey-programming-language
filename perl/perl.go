6cc52b376addc95ee38313772d5f4d27494df3a3
petrostrak <pit.trak@gmail.com>
2021-12-18 10:55:32 +0200

    Update Start function and Add printParseErrors

--- perl/perl.go
+++ perl/perl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/petrostrak/the-monkey-programming-language/lexer"
-	"github.com/petrostrak/the-monkey-programming-language/token"
+	"github.com/petrostrak/the-monkey-programming-language/parser"
 )
 
 const PROMPT = ">> "
@@ -26,9 +26,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
