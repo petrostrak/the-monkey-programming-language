60b9abaaf262a1b0e00f9b6a4a3e42e0e718c602
petrostrak <pit.trak@gmail.com>
2021-12-19 13:06:13 +0200

    Add TestEvalIntegerExpression method

---
+++ evaluator/evaluator_test.go
@@ -0,0 +1,47 @@
+package evaluator
+
+import (
+	"testing"
+
+	"github.com/petrostrak/the-monkey-programming-language/lexer"
+	"github.com/petrostrak/the-monkey-programming-language/object"
+	"github.com/petrostrak/the-monkey-programming-language/parser"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	}
+
+	for _, tt := range testCases {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New(input)
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Object)
+	if !ok {
+		t.Errorf("object is not Integer; got %T (%+v)", obj, obj)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("object has wrong value; got %d, want %d", result.Value, expected)
+		return false
+	}
+
+	return true
+}
