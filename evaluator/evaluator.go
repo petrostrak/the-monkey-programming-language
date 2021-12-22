c47ce14e7dccf2b95e8e94ede83109ad59f862da
petrostrak <pit.trak@gmail.com>
2021-12-22 22:16:27 +0200

    Udd ast.FunctionLiteral case in Eval func

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -36,6 +36,10 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
+	case *ast.FunctionLiteral:
+		params := node.Parameters
+		body := node.Body
+		return &object.Function{Parameters: params, Body: body, Env: env}
 
 	// Expressions
 	case *ast.IntegerLiteral:
