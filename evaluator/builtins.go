44f8f4780600937841a316f2fbd7e0dd4d7e2923
petrostrak <pit.trak@gmail.com>
2021-12-23 20:12:56 +0200

    Implement builtin function len

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,11 +1,22 @@
 package evaluator
 
-import "github.com/petrostrak/the-monkey-programming-language/object"
+import (
+	"github.com/petrostrak/the-monkey-programming-language/object"
+)
 
 var buildins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			if len(args) != 1 {
+				return newError("wrong number of arguments; got %d, want 1", len(args))
+			}
+
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported; got %s", args[0].Type())
+			}
 		},
 	},
 }
