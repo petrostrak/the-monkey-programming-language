0746708a32d82e1205c59ba65f7064b162e7646b
petrostrak <pit.trak@gmail.com>
2022-01-23 16:36:40 +0200

    Update repl.Start

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/petrostrak/the-monkey-programming-language/compiler"
 	"github.com/petrostrak/the-monkey-programming-language/lexer"
+	"github.com/petrostrak/the-monkey-programming-language/object"
 	"github.com/petrostrak/the-monkey-programming-language/parser"
 	"github.com/petrostrak/the-monkey-programming-language/vm"
 )
@@ -18,6 +19,10 @@ const PROMPT = ">> "
 // all the tokens the lexer gives us until we encounter EOF.
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
+	constants := []object.Object{}
+	globals := make([]object.Object, vm.GlobalsSize)
+	symbolTable := compiler.NewSymbolTable()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -36,14 +41,15 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		comp := compiler.New()
+		comp := compiler.NewWithState(symbolTable, constants)
 		err := comp.Compile(program)
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Compilation failed:\n %s\n", err)
 			continue
 		}
-
-		machine := vm.New(comp.Bytecode())
+		code := comp.Bytecode()
+		constants = code.Constants
+		machine := vm.NewWithGlobalsStore(code, globals)
 		err = machine.Run()
 		if err != nil {
 			fmt.Fprintf(out, "Woops! Executing bytecode failed:\n %s\n", err)
