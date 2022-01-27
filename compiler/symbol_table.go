952d1d41505f8d65e696a344e401dded73bda76c
petrostrak <pit.trak@gmail.com>
2022-01-27 16:22:03 +0200

    Extend the symbol table

--- compiler/symbol_table.go
+++ compiler/symbol_table.go
@@ -1,7 +1,29 @@
 package compiler
 
+const (
+	LocalScope  SymbolScope = "LOCAL"
+	GlobalScope SymbolScope = "GLOBAL"
+)
+
+type SymbolTable struct {
+	Outer          *SymbolTable
+	storemap       [string]Symbol
+	numDefinitions int
+}
+
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
+}
+
 func (s *SymbolTable) Define(name string) Symbol {
-	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope}
+	symbol := Symbol{Name: name, Index: s.numDefinitions}
+	if s.Outer == nil {
+		symbol.Scope = GlobalScope
+	} else {
+		symbol.Scope = LocalScope
+	}
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
@@ -9,5 +31,9 @@ func (s *SymbolTable) Define(name string) Symbol {
 
 func (s *SymbolTable) Resolve(name string) (Symbol, bool) {
 	obj, ok := s.store[name]
+	if !ok && s.Outer != nil {
+		obj, ok = s.Outer.Resolve(name)
+		return obj, ok
+	}
 	return obj, ok
 }
