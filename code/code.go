b625f5318fb779c97a7b3774de888eb7df975b7a
petrostrak <pit.trak@gmail.com>
2022-01-27 16:07:23 +0200

    Add Opcodes for local bindings

--- code/code.go
+++ code/code.go
@@ -35,6 +35,8 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 // The Definition of an Opcode has two fields. Name helps
@@ -71,6 +73,8 @@ var (
 		OpCall:          {"OpCall", []int{}},
 		OpReturnValue:   {"OpReturnValue", []int{}},
 		OpReturn:        {"OpReturn", []int{}},
+		OpGetLocal:      {"OpGetLocal", []int{1}},
+		OpSetLocal:      {"OpSetLocal", []int{1}},
 	}
 )
 
@@ -103,10 +107,11 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
-
 	return instruction
 }
 
@@ -151,18 +156,19 @@ func ReadUint16(ins Instructions) uint16 {
 	return binary.BigEndian.Uint16(ins)
 }
 
+func ReadUint8(ins Instructions) uint8 { return uint8(ins[0]) }
+
 func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 	operands := make([]int, len(def.OperandWidths))
 	offset := 0
-
 	for i, width := range def.OperandWidths {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
-
 		offset += width
 	}
-
 	return operands, offset
 }
